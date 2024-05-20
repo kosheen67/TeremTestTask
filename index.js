6dc4e0c82db6aef1102815b2e07cdc1f0d8571e3
kosheen67 <kosheen67@gmail.com>
2024-05-20 14:19:46 +0300

    add html and css with form

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert("Привет, мир")
+alert("Привет, мир");
+
+let block = document.querySelector('.block1');
+let button = document.getElementById('yellow-button');
+
+button.addEventListener("click", function () {
+    if (block.style.display === "none") {
+        block.style.display = "block";
+    } else {
+        block.style.display = "none";
+    }
+})
+
+
