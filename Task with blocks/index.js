bbc67fdee0e4f777ff43a23c2c8f7e408c32be71
kosheen67 <kosheen67@gmail.com>
2024-05-21 14:17:32 +0300

    fix code with swaping blocks

--- Task with blocks/index.js
+++ Task with blocks/index.js
@@ -1,4 +1,4 @@
-/*alert("Привет, мир");*/
+alert("Привет, мир");
 
 let block = document.querySelector('.block1');
 let button = document.getElementById('yellow-button');
@@ -20,6 +20,9 @@ let block3 = document.querySelector('.block2-brown');
 let container = document.querySelector('.block2');
 
 button2.addEventListener("click", function () {
-    container.insertBefore(block3, block2);
+    if (block2.nextSibling === block3) {
+        container.insertBefore(block3, block2);
+    }
+    else { container.insertBefore(block2, block3); }
 
 });
